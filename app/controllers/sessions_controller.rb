118369e92f016b66b82ca9b9eda5bccdcd1449fa
Dan Wagner <danwagnerco@gmail.com>
2014-08-10 19:10:40 -0500

    finish day 6 sign out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,4 +15,9 @@ class SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, :notice => "Now you're signed out!"
+  end
+
 end
