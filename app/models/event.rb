18bbf0c589229749f10beb279f646686c13a4828
Dan Wagner <danwagnerco@gmail.com>
2014-08-19 17:58:06 -0500

    finish day 13 through associations part 1

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,20 +1,17 @@
 class Event < ActiveRecord::Base
+  has_many :registrations, dependent: :destroy
+  has_many :likes, :dependent => :destroy
+  has_many :likers, :through => :likes, :source => :user
+
   validates :name, presence: true
-
   validates :location, presence: true
-  
   validates :description, length: { minimum: 25 }
-
   validates :price, numericality: { greater_than_or_equal_to: 0 }
-
   validates :capacity, numericality: { only_integer: true, greater_than: 0 }
-
   validates :image_file_name, allow_blank: true, format: {
     with:    /\w+\.(gif|jpg|png)\z/i,
     message: "must reference a GIF, JPG, or PNG image"
   }
-    
-  has_many :registrations, dependent: :destroy
   
   def self.upcoming
     where('starts_at >= ?', Time.now).order(:starts_at)
